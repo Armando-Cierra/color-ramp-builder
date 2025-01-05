aacadf18af4470dd11c77bac1586029e16c0acd5
Armando-Cierra <armando_cierra@outlook.com>
2025-01-05 17:45:42 -0600

    ✨ error page

--- src/views/Error404/Error404.tsx
+++ src/views/Error404/Error404.tsx
@@ -1,7 +1,31 @@
+import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { motion } from 'framer-motion';
+import { Button } from '@/components';
+import './error.scss';
+
 export const Error404 = () => {
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>Error 404</h1>
-    </div>
+    <motion.div
+      initial={{
+        opacity: 0,
+      }}
+      animate={{
+        opacity: 1,
+      }}
+      exit={{
+        opacity: 0,
+      }}
+      className="error"
+    >
+      <h1>404</h1>
+      <p>{t('error.description')}</p>
+      <Button variant="contrast" onClick={() => navigate('/')}>
+        {t('error.goHome')}
+      </Button>
+    </motion.div>
   );
 };
